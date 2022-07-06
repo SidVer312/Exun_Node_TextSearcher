143a8f588cbac97268ec2457a82881d991cbe75d
Siddhartha Verma <57825216+SidVer312@users.noreply.github.com>
2022-07-06 17:11:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+//import fs
+const fs = require('fs');
+const input = process.argv[2];
+
+const word = input.split(' ');
+fs.readFile('./example.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data.includes(word));
+});
+
